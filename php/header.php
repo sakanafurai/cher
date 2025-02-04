570eec1d23201230a125e2c761c2383fd07541d6
sakanafurai <info@via.rossa.cc>
2025-02-04 19:33:11 +0900

    Add title image

--- php/header.php
+++ php/header.php
@@ -1,5 +1,13 @@
 <header>
-  <h1 class="title"><a href="<?php echo Theme::siteUrl() ?>"><?php echo $site->title(); ?></a></h1>
+  <div class="header-logo">
+  <?php if ($site->logo()): ?>
+    <figure class="title-logo">
+      <img class="image" src="<?php echo ($site->logo()); ?>" alt="<?php echo $site->title(); ?>">
+    </figure>
+  <?php else: ?>
+    <h1 class="title"><a href="<?php echo Theme::siteUrl() ?>"><?php echo $site->title(); ?></a></h1>
+  <?php endif ?>
+  </div>
 
   <nav class="navbar">
    <button class="navbar-btn" id="navbar-btn">MENU<br><span class="arrow"></span></button>
