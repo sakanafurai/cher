244846bfc41a59ac113182011d8f12a0816a9c8d
sakanafurai <info@via.rossa.cc>
2025-02-12 15:09:25 +0900

    Add icons to category and tag list title

--- php/home.php
+++ php/home.php
@@ -1,6 +1,6 @@
   <?php if ( $WHERE_AM_I == 'category' ): ?>
     <div class="category-title">
-      <h2 class="title"><?php echo $page->category(); ?></h2>
+      <h2 class="title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder"><path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z"/></svg><?php echo $page->category(); ?></h2>
     </div>
   <?php endif ?>
 
@@ -14,7 +14,7 @@
     $tag = new Tag($tagKey);
 
     // Print the tag name
-    echo '<div class="category-title"><h2 class="title">'. $tag->name() . '</h2></div>';
+    echo '<div class="category-title"><h2 class="title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tag"><path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z"/><circle cx="7.5" cy="7.5" r=".5"/></svg>'. $tag->name() . '</h2></div>';
   }
   ?>
 
